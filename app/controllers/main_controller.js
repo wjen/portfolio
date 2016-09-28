df892a79b901c6b74a7f5f55f4badf60fbaaf546
Wen Jen <wjen85@gmail.com>
2016-09-28 11:02:58 -0700

    add anchorscroll

--- app/controllers/main_controller.js
+++ app/controllers/main_controller.js
@@ -2,12 +2,24 @@
   'use strict';
 
   angular.module("Portfolio")
-    .controller("MainController", MainController);
-
-    MainController.$inject = [];
-
-    function MainController() {
-      var vm = this;
-    }
+    .run(['$anchorScroll', function($anchorScroll) {
+      $anchorScroll.yOffset = 70;   // always scroll by 50 extra pixels
+    }])
+    .controller("MainController", ['$scope', '$location', '$anchorScroll',
+      function($scope, $location, $anchorScroll) {
+        $scope.gotoPortfolio = function() {
+          $location.hash('portfolio');
+          $anchorScroll();
+        };
+        $scope.gotoAbout = function() {
+          $location.hash('about');
+          $anchorScroll();
+        };
+        $scope.gotoContact = function() {
+          $location.hash('contact');
+          $anchorScroll();
+        };
+      }
+    ]);
 
 })();
--- app/js/app.routes.js
+++ app/js/app.routes.js
@@ -13,28 +13,28 @@
         url: "/",
         templateUrl: "./app/templates/home.html",
         controller: "MainController",
-        controllerAs: "vm"
+
       })
       .state("wanderPage",
       {
         url: "/",
         templateUrl: "./app/templates/wander.html",
         controller: "MainController",
-        controllerAs: "vm"
+
       })
         .state("instaTweetPage",
         {
         url: "/",
         templateUrl: "./app/templates/instaTweet.html",
         controller: "MainController",
-        controllerAs: "vm"
+
       })
         .state("blackjackPage",
         {
         url: "/",
         templateUrl: "./app/templates/blackjack.html",
         controller: "MainController",
-        controllerAs: "vm"
+
       });
 
 ;
